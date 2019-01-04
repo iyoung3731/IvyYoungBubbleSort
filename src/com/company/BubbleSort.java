0d73b80cb3d6fd368849fe3b28fa243b2f84b6c3
Ivy Young <iyoung3731@bths.edu>
2019-01-04 08:38:05 -0500

    argj

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -1,14 +1,22 @@
 package com.company;
 
 public class BubbleSort {
-    public static void bubbleSort(int[] arr){
-        while(){
-            int temp = arr[i];
-            arr[j] = arr[i];
-            arr[j] = temp;
-        }
-
-
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[j] = arr[i];
+        arr[j] = temp;
     }
+    public static void bubbleSort(int[] arr){
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] > arr[i+1]){
+                swap(arr, arr[i], arr[i+1]);
+            }
+        }
+    }
+    public static int[] randIntArr(int count){}
+
+    public static void isSorted(int[] arr){}
+
+    public static void checkSum(int[] before, int[] after){}
 
 }
